63eea779cffbe235597cc45d50606ac67252d3ea
pashwa-python-bash <67837545+pashwa-python-bash@users.noreply.github.com>
2020-07-04 18:25:00 +0100

    Add files via upload

---
+++ python name, age and year excercise.py
@@ -0,0 +1,5 @@
+name= input("What is your full name and middle name if you have one?")
+age= input("How old are you?")
+year= input("what year are you in?")
+print(f" Oooh.. I like your name, {name}  ,it's a unique name" + " and your age is {age} ,i wish i was that age" + "and you are in " {year} +
+      " that is a fun year")
