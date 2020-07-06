e0340a8f14b599ab74b663e0a6a0c6241a11a673
pashwa-python-bash <67837545+pashwa-python-bash@users.noreply.github.com>
2020-07-06 18:29:04 +0100

    Add files via upload

---
+++ spiral 1.py
@@ -0,0 +1,11 @@
+import turtle
+spiral = turtle.Turtle()
+spiral.color("blue")
+turtle.speed(0)
+for i in range (300):
+    spiral.forward(i)
+    spiral.right(75)
+
+
+turtle.done()
+               
---
+++ spiral 2.py
@@ -0,0 +1,11 @@
+import turtle
+spiral = turtle.Turtle()
+spiral.color("blue")
+turtle.speed(0)
+for i in range (300):
+    spiral.forward(i)
+    spiral.right(244)
+
+
+turtle.done()
+               
