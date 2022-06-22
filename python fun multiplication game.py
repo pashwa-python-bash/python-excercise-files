cff75bcc0cef4df169d86eea36c54f848ca7a8b8
pashwa-python-bash <67837545+pashwa-python-bash@users.noreply.github.com>
2022-06-22 09:43:23 +0100

    Update python fun multiplication game.py

--- python fun multiplication game.py
+++ python fun multiplication game.py
@@ -1,41 +1,43 @@
 import time
 import random
+q1 = "yes"
 
 start = input("Do you want to play the multiplication game:  (yes/no):  ").lower()
-
-if start == "yes":
-  correct = 0
-  questions = 0
-
-  delay = int(input("\nHow long do you want to answer as many questions as you can(type ur answer in seconds):  "))
-  end = time.time() + delay
-
+while q1 == "yes":
+  if start == "yes":
+    correct = 0
+    questions = 0
   
-
-
-
-  while time.time() < end:
-      n1 = random.randint(2,12)
-      n2 = random.randint(2,12)
- 
-      answer  = int(input(str(n1) +  "x" + str(n2) + "=  "))
-      questions +=1
-    
-      if answer == n1 * n2:
-        correct +=1
-        print("Correct!")
-
-      
-      
-      else:
-        print("Incorrect!")
-
-  print("\nTime's up!")
-  time.sleep(2)
-  print(f"\nYou got {correct} correct out of {questions} questions.")
+    delay = int(input("\nHow long do you want to answer as many questions as you can(type ur answer in seconds):  "))
+    end = time.time() + delay
   
-elif start == "no":
-  print("Thanks for coming anyways!")
-else:
-  print("Invalid Option!")
+   
+  
+  
+  
+    while time.time() < end:
+        n1 = random.randint(2,12)
+        n2 = random.randint(2,12)
+   
+        answer  = int(input(str(n1) +  "x" + str(n2) + "=  "))
+        questions +=1
+     
+        if answer == n1 * n2:
+          correct +=1
+          print("\nCorrect!")
+  
+       
+       
+        else:
+          print("\nIncorrect!")
+  
+    print("\nTime's up!")
+    time.sleep(2)
+    print(f"\nYou got {correct} correct out of {questions} questions.")
+    q1 = input("Do you want to play again (yes/no):  ").lower()
+   
+  elif start == "no":
+    print("Thanks for coming anyways!")
+  else:
+    print("Invalid Option!")
   
